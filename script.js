e0a9b2a3ee6864fade6ec972367e07ce15276245
CHARLLYS97 <jhonnycharllys@hotmail.com>
2024-05-17 18:37:36 -0300

    Manipulando o DOM - Validação do horário de funcionamento, Envio da API para o WhatsApp

--- script.js
+++ script.js
@@ -91,14 +91,14 @@ function updateCartModal() {
    `
 
         total += item.price * item.quantity;
-        
+
         cartItemsContainer.appendChild(cartItemElement)
 
     })
 
-    cartTotal.textContent = total.toLocaleString("PT-BR",{
-    style: "currency",
-    currency: "BRL"    
+    cartTotal.textContent = total.toLocaleString("PT-BR", {
+        style: "currency",
+        currency: "BRL"
     });
 
     cartCounter.innerHTML = cart.length;
@@ -107,47 +107,105 @@ function updateCartModal() {
 
 
 // Função para remover item do carrinho 
-cartItemsContainer.addEventListener("click", function (event){
-    if(event.target.classList.contains("remove-from-cart-btn")){
+cartItemsContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("remove-from-cart-btn")) {
         const name = event.target.getAttribute("data-name")
 
-      removeItemCart(name);
+        removeItemCart(name);
     }
 })
 
-function removeItemCart(name){
+function removeItemCart(name) {
     const index = cart.findIndex(item => item.name === name);
 
-    if(index !== -1){
+    if (index !== -1) {
         const item = cart[index];
-      
-       if(item.quantity > 1){
-        item.quantity -= 1;
-        updateCartModal();
-        return;
-       } 
 
-       cart.splice(index, 1);
-       updateCartModal();
+        if (item.quantity > 1) {
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCartModal();
 
     }
 }
 
 
-addressInput.addEventListener("input", function(event){
+addressInput.addEventListener("input", function (event) {
     let inputValue = event.target.value;
 
-   if(inputValue !== ""){
-    addressInput.classList.remove("border-red-500")
-    addressWarn.classList.add("hidden")
-   }
+    if (inputValue !== "") {
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
 })
 
-checkoutBtn.addEventListener("click", function(){
-    if(cart.length === 0) return;
-    if(addressInput.value === ""){
+
+// Finalizar pedido 
+checkoutBtn.addEventListener("click", function () {
+
+    const isOpen = checkRestaurantOpen()
+    if (!isOpen) {
+
+        Toastify({
+            text: "Ops, o restaurante está fechado!",
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#EF4444",
+            },
+        }).showToast();
+
+        return;
+    }
+
+    if (cart.length === 0) return;
+    if (addressInput.value === "") {
         addressWarn.classList.remove("hidden")
         addressInput.classList.add("border-red-500")
         return;
     }
+
+    //Enviar o pedido para a API whatsApp
+    const cartItems = cart.map((item) => {
+        return (
+            ` ${item.name}Quantidade: (${item.quantity}) Preço: R$${item.price} |`
+        )
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "5511954373573"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+    cart = [];
+    updateCartModal();
+
+
 })
+
+
+// Verificar a hora manipular o card de acordo com o horário
+function checkRestaurantOpen() {
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+    //true = restaurante está aberto
+}
+
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if (isOpen) {
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-600")
+} else {
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+}
