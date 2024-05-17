7d561ffa56c9964632d50a8ae3cd6fee8f9ca498
CHARLLYS97 <jhonnycharllys@hotmail.com>
2024-05-17 16:09:03 -0300

    Manipulando o DOM - Adicionar e verificar endereço

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const checkoutBtn = document.getElementById("checkout-btn")
 const closeModalBtn = document.getElementById("close-modal-btn")
 const cartCounter = document.getElementById("cart-count")
 const addressInput = document.getElementById("address")
-const addressWarn = document.getElementById("adress-warn")
+const addressWarn = document.getElementById("address-warn")
 
 let cart = [];
 
@@ -132,3 +132,22 @@ function removeItemCart(name){
 
     }
 }
+
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+   if(inputValue !== ""){
+    addressInput.classList.remove("border-red-500")
+    addressWarn.classList.add("hidden")
+   }
+})
+
+checkoutBtn.addEventListener("click", function(){
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+})
